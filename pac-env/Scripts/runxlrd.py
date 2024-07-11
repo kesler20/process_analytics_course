daedc5e41e292baac4c820ed4e6d947e651df9ab
kesler20 <uchekesla@gmail.com>
2024-07-11 21:18:59 +0100

    chore: make it better🪴

--- pac-env/Scripts/runxlrd.py
+++
@@ -1,4 +0,0 @@
-#!C:\Users\Uchek\protocol\process_analytics_course\pac-env\Scripts\python.exe
-# EASY-INSTALL-SCRIPT: 'xlrd==2.0.1','runxlrd.py'
-__requires__ = 'xlrd==2.0.1'
-__import__('pkg_resources').run_script('xlrd==2.0.1', 'runxlrd.py')
